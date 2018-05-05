441192383a40be5af36ad09c881ba86b3dbb45df
Jonatas Reis <jonatassreis13@gmail.com>
2018-05-05 00:11:52 -0400

    Foi feito um teste para aprender a alterar o gráfico dinamicamente.

--- app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
+++ app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
@@ -1,27 +1,72 @@
 package com.example.jonatas.googlechartslearn;
 
+import android.annotation.SuppressLint;
 import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private WebView wb;
+    @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
          wb = findViewById(R.id.webCharts);
-        WebSettings ws = wb.getSettings();
-        ws.setJavaScriptEnabled(true);
-
+        wb.getSettings().setJavaScriptEnabled(true);
+        wb.addJavascriptInterface(this,"Android");
         wb.loadUrl("file:///android_asset/testepizza.html");
     }
 
+    public void teste(View view){
+        /*Log.d("Jonatas","kjkjhjk");
+        String nomes[] = new String[]{"Jonatas","Teste","Manoel","Lucia"};
+        final JSONArray jsonArray = new JSONArray();
+        try {
+            jsonArray.put(0,"Jonatas");
+            jsonArray.put(1,"Teste");
+            jsonArray.put(2,"Manoel");
+            jsonArray.put(3,"Lucia");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                wb.loadUrl("javascript:metodoWebView2();");
+                //wb.loadUrl("javascript:drawChart();");
+            }
+        });
 
+    }
+
+    @JavascriptInterface
+    public String metodoPonte(){
+        String nomes[] = new String[]{"Jonatas","Teste","Manoel","Lucia"};
+        final JSONArray jsonArray = new JSONArray();
+        try {
+            jsonArray.put(0,"Jonatas");
+            jsonArray.put(1,"Teste");
+            jsonArray.put(2,"Manoel");
+            jsonArray.put(3,"Lucia");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("TesteJ",jsonArray.toString());
+        return jsonArray.toString();
+
+    }
 }
