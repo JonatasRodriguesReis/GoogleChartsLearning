55b764174ada24a73fd5c8e9326387cefea13a4b
Jonatas Reis <jonatassreis13@gmail.com>
2018-05-08 22:30:48 -0400

    Foi feito um teste para aprender a alterar o gráfico dinamicamente.

--- app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
+++ app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
@@ -25,17 +25,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         wb = findViewById(R.id.webCharts);
+        wb = findViewById(R.id.webCharts);
         wb.getSettings().setJavaScriptEnabled(true);
         wb.addJavascriptInterface(this,"Android");
         wb.loadUrl("file:///android_asset/testepizza.html");
     }
 
     public void teste(View view){
+        final String t = "Jonatas Top";
+        String nomes[] = new String[]{"Jonatas","5","Teste","3","Manoel","77","Lucia","48"};
+        final JSONArray jsonArray = new JSONArray();
+        try {
+            for (int i = 0; i < nomes.length; i++) {
+                jsonArray.put(i,nomes[i]);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                wb.loadUrl("javascript:metodoWebView2();");
+
+                wb.loadUrl("javascript:metodoWebView2('" + jsonArray.toString() +"');");
             }
         });
 
