21575588dc678c14b5081e4bd1697b2c5645ccba
Jonatas Reis <jonatassreis13@gmail.com>
2018-05-10 19:41:58 -0400

    Foi feito um teste para aprender a alterar o gráfico dinamicamente.

---
+++ app/src/main/java/model/DataValor.java
@@ -0,0 +1,14 @@
+package model;
+
+public class DataValor {
+    private String data;
+    private String valor;
+
+    public String getData() {
+        return data;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
