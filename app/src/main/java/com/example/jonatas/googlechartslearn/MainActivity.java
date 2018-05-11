dc854c1e4bde86cb6c3976158fa2d51928a5a61d
Jonatas Reis <jonatassreis13@gmail.com>
2018-05-10 21:24:42 -0400

    Foi feito um teste para aprender a alterar o gráfico dinamicamente.

--- app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
+++ app/src/main/java/com/example/jonatas/googlechartslearn/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void teste(View view){
-        /*final String t = "Jonatas Top";
+        final String t = "Jonatas Top";
         String nomes[] = new String[]{"Jonatas","5","Teste","3","Manoel","77","Lucia","48"};
         final JSONArray jsonArray = new JSONArray();
         try {
@@ -55,10 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
                 wb.loadUrl("javascript:metodoWebView2('" + jsonArray.toString() +"');");
             }
-        });*/
+        });
 
+        metodoPonte();
+    }
 
+    @JavascriptInterface
+    public void metodoPonte(){
         AsyncHttpClient client = new AsyncHttpClient();
+
         client.get("http://" + getResources().getString(R.string.ip) + ":85/webserviceCharts/data/getAll.php",null,new JsonHttpResponseHandler(){
 
             @Override
@@ -66,12 +71,13 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     final JSONArray datas = response.getJSONArray("records");
 
-                    runOnUiThread(new Runnable() {
+                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            wb.loadUrl("javascript:metodoWebView2('" + datas.toString() +"');");
+                            wb.loadUrl("javascript:metodo('" + datas.toString() +"');");
                         }
                     });
+
                 }  catch (JSONException e) {
                     e.printStackTrace();
 
@@ -79,22 +85,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-    }
-
-    @JavascriptInterface
-    public String metodoPonte(){
-        String nomes[] = new String[]{"Jonatas","5","Teste","3","Manoel","77","Lucia","48"};
-        final JSONArray jsonArray = new JSONArray();
-        try {
-            for (int i = 0; i < nomes.length; i++) {
-                jsonArray.put(i,nomes[i]);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return jsonArray.toString();
-
     }
 }
