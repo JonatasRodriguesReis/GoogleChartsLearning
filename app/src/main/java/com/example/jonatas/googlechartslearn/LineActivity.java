a8cfcd5b0074bf370b591ee03bc3460963c28ff4
Jonatas Reis <jonatassreis13@gmail.com>
2018-05-24 21:16:13 -0400

    Foi feito um teste para aprender a alterar o gráfico dinamicamente.

--- app/src/main/java/com/example/jonatas/googlechartslearn/LineActivity.java
+++ app/src/main/java/com/example/jonatas/googlechartslearn/LineActivity.java
@@ -1,17 +1,12 @@
 package com.example.jonatas.googlechartslearn;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.webkit.JavascriptInterface;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -19,24 +14,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
-import model.DataValor;
-
-public class MainActivity extends AppCompatActivity {
+public class LineActivity extends AppCompatActivity {
     private WebView wb;
     @SuppressLint("JavascriptInterface")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_line);
 
         wb = findViewById(R.id.webCharts);
         wb.getSettings().setJavaScriptEnabled(true);
         wb.addJavascriptInterface(this,"Android");
-        wb.loadUrl("file:///android_asset/testepizza.html");
+        wb.loadUrl("file:///android_asset/testelinha.html");
     }
 
     public void teste(View view){
@@ -58,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        metodoPonte();
+        //metodoPonte();
     }
 
     @JavascriptInterface
-    public void metodoPonte(){
+    public void metodoPonte(View view){
         AsyncHttpClient client = new AsyncHttpClient();
 
         client.get("http://" + getResources().getString(R.string.ip) + ":85/webserviceCharts/data/getAll.php",null,new JsonHttpResponseHandler(){
@@ -72,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     final JSONArray datas = response.getJSONArray("records");
 
-                     runOnUiThread(new Runnable() {
+                    runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             wb.loadUrl("javascript:metodo('" + datas.toString() +"');");
@@ -86,10 +75,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    public void verCurva(View view){
-        Intent intent = new Intent(MainActivity.this,LineActivity.class);
-        startActivity(intent);
     }
 }
